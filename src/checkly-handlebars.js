05c9f16a93c1c1258d49cab479564f084f51bd1a
Antoine Coutellier <coutellier.antoine@gmail.com>
2022-07-07 16:21:11 +0200

    feat: add new handlebars method to get and fresh instance [sc-00] (#80)

--- src/checkly-handlebars.js
+++ src/checkly-handlebars.js
@@ -3,9 +3,15 @@ import helpers from 'handlebars-helpers'
 import { v4 } from 'uuid'
 import random from 'lodash.random'
 
-const handlebars = Handlebars.create()
-handlebars.registerHelper('$RANDOM_NUMBER', () => random(0, 10000))
-handlebars.registerHelper('$UUID', () => v4())
-handlebars.registerHelper(helpers(['comparison', 'date']))
+const create = () => {
+  const handlebars = Handlebars.create()
+  handlebars.registerHelper('$RANDOM_NUMBER', () => random(0, 10000))
+  handlebars.registerHelper('$UUID', () => v4())
+  handlebars.registerHelper(helpers(['comparison', 'date']))
 
-export default handlebars
+  return handlebars
+}
+
+const handlebars = create()
+
+export default {...handlebars, create }
